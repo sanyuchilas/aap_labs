0733935d7c284d5d9e4b6f6430919c0937cbe48f
sanyuchilas <sanyuchilas@gmail.com>
2024-04-10 11:57:03 +0300

    initiated lab 10

---
+++ 10/10_Keyer_BAM231.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Input/output library.
+#include <stdlib.h> // Memory allocation.
+#include <time.h> // Time library.
+#include <assert.h> // Assertion library.
+#include <string.h> // String functions library.
+
+
+int main() {
+  return 0;
+}
