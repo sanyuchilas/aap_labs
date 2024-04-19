6c81aff1e21394c3cb0bc14092781b5dfc34cc3e
sanyuchilas <sanyuchilas@gmail.com>
2024-04-20 00:03:15 +0300

    added string validation and input

--- 10/10.2_Keyer_BAM23.c
+++ 10/10.2_Keyer_BAM23.c
@@ -3,39 +3,37 @@
 #include <assert.h> // Assertion library.
 #include <string.h> // String functions library.
 
-// Useful directives for string tokens.
-#define separator ' '
-#define endToken '.'
-
 // Function validating a string.
-void isSValid(char* s) {
-    // Checking string for incorrect symbols.
+void isValid(char* s) {
     for (int i = 0; s[i] != 0; i++) {
-        if (s[i] < 'a' || s[i] > 'b') {
-            printf("The string contains incorrect symbols.\n");
-            assert(s[i] >= 'a' && s[i] <= 'z');
+        if (s[i] < 'a' || s[i] > 'z') {
+            printf("String contains incorrect symbol: %c.\n", s[i]);
+            assert(s[i] >= 'a' || s[i] <= 'z');
         }
     }
 }
 
 // Function reading a string from stdin.
-int readingStringFromUser() {
-  printf("Please, enter correct string:\n");
+char* readingStringFromUser() {
+  printf("Please, enter correct string: ");
 
   char* s = (char*)malloc(sizeof(char) * 100);
 
   fflush(stdin);
   fgets(s, 100, stdin);
-  s[strcspn(s, endToken)] = 0;
+  s[strcspn(s, "\n")] = 0;
+  s[strcspn(s, ".")] = 0;
 
   return s;
 }
 
 int main() {
     char* s = readingStringFromUser();
-    isSValid(s);
+    isValid(s);
 
     printf("Work with string: %s", s);
+
+    
 
     free(s);
 }
