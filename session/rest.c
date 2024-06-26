6fbea3d2cf2b173f24d7b014530cde8a489b087e
sanyuchilas <sanyuchilas@gmail.com>
2024-06-26 09:32:10 +0300

    change

---
+++ session/rest.c
@@ -0,0 +1,9 @@
+void sum(int a1, ...);
+
+void sum(int a1, ...) {
+
+}
+
+int main() {
+    return 0;
+}
--- session/session.c
+++ session/session.c
@@ -2,30 +2,35 @@
 
 int main() {
     // Question 1.
-    char var; // 8 bit = 1 byte, [-128, 127]
-    signed char var; // 8 bit = 1 byte, [-128, 127]
-    unsigned char var; // 8 bit = 1 byte, [0, 256]
+    // char var; // 8 bit = 1 byte, [-128, 127]
+    // signed char var; // 8 bit = 1 byte, [-128, 127]
+    // unsigned char var; // 8 bit = 1 byte, [0, 256]
 
-    short int var; // 16 bit = 2 byte
-    signed short int var;
-    unsigned short int var;
+    // short int var; // 16 bit = 2 byte
+    // signed short int var;
+    // unsigned short int var;
 
-    int var; // 32 bit = 4 byte
-    signed int var;
-    unsigned int var;
+    // int var; // 32 bit = 4 byte
+    // signed int var;
+    // unsigned int var;
 
-    long int var; // >= 32 bit = 4 byte
-    signed long int var;
-    unsigned long int var;
+    // long int var; // >= 32 bit = 4 byte
+    // signed long int var;
+    // unsigned long int var;
 
-    long long int  var; // 64 bit = 4 byte
-    signed long long int var;
-    unsigned long long int var;
+    // long long int  var; // 64 bit = 4 byte
+    // signed long long int var;
+    // unsigned long long int var;
 
-    double var;
-    long double var;
+    // double var;
+    // long double var;
 
-    const char var; // The constant, but we also have a #define directive
+    // const char var; // The constant, but we also have a #define directive
+
+    char str[80];
+    sprintf(str, "%s %d %c", "one", 2, '3');
+
+    printf("%s", str);
 
     return 0;
-}
+};
